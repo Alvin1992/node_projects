b32e26064471c463dfc6dba3cc74028b2ea17532
Alvin <fanrx_1992@163.com>
2016-09-05 10:49:16 +0800

    模块内的变量

---
+++ use_module/test_variables.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Alvin on 2016/9/5.
+ */
+
+console.log('require: ', require);
+console.log('require.extensions: ', require.extensions);
+console.log('module: ', module);
+console.log('exports: ', exports);
+console.log('_filename: ', __filename);
+console.log('_dirname: ', __dirname);
