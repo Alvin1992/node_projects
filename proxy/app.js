1d3c6236f7c91cea608be9e69bcf566aaed77ba7
Alvin <fanrx_1992@163.com>
2016-09-08 20:07:54 +0800

    并发基础版本

---
+++ proxy/app.js
@@ -0,0 +1,58 @@
+/**
+ * Created by Alvin on 2016/9/8.
+ */
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var url = require('url');
+var eventproxy = require('eventproxy');
+var fs = require('fs');
+
+var cnodeUrl = 'https://cnodejs.org/';
+
+superagent.get(cnodeUrl).end((err, res) => {
+    if ( err ) {
+        return console.error(err);
+    }
+
+    var topicUrls = [];
+    var $ = cheerio.load(res.text);
+    $('#topic_list .topic_title').each((idx, elem) => {
+        var $elem = $(elem);
+        var href = url.resolve(cnodeUrl, $elem.attr('href'));
+        topicUrls.push(href);
+    });
+    
+    //console.log(topicUrls);
+    var ep = new eventproxy();
+
+    ep.after('getTopicInfo', 3, (topics) => {
+        topics = topics.map((topicPair) => {
+            var topicUrl = topicPair[0];
+            var topicHtml = topicPair[1];
+            var $ = cheerio.load(topicHtml);
+            return ({
+                title: $('.topic_full_title').text().trim(),
+                href: topicUrl,
+                comment1: $('#reply1').find('.markdown-text p').text().trim(),
+                author1: $('#reply1').find('.user_info .reply_author').text().trim()
+            })
+        });
+
+        var content = JSON.stringify(topics, null, 4);
+        fs.writeFile('result.json', content, (err) => {
+            if ( err ) {
+                console.error(err);
+            } else {
+                console.log('It\'s saved!');
+            }
+        });
+    });
+
+    topicUrls.length = 3;
+    topicUrls.forEach((tUrl) => {
+        superagent.get(tUrl).end((err, res) => {
+            console.log('fetch ' + tUrl + ' successful');
+            ep.emit('getTopicInfo', [tUrl, res.text]);
+        });
+    });
+});
