fc7a4cee76682548357b907e562338f84ea0136c
Alvin1992 <fanrx_1992@163.com>
2016-09-10 19:55:35 +0800

    test file of jsdoc

---
+++ test_jsdoc.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alvin on 9/10/16.
+ */
+
+/**
+ * test for jsdoc
+ * @param {string} foo - The first param
+ * @param {string} bar - The second param*/
+function foobar(foo, bar) {
+
+}
